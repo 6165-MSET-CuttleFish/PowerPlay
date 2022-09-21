d6561620c8008481b9880946ea4edc9c66d8ad6a
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2022-09-20 20:53:37 -0700

    drivetrain setup done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,11 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 public class Robot extends SampleMecanumDrive {
-    public Robot(HardwareMap hardwareMap) {
-        super(hardwareMap);
+    public DcMotorEx fl, bl, fr, br;
+    private final HardwareMap hardwareMap;
+    public Robot(OpMode opMode) {
+        super(opMode.hardwareMap);
+        hardwareMap = opMode.hardwareMap;
+        fl = hardwareMap.get(DcMotorEx.class, "fl");
+        fr = hardwareMap.get(DcMotorEx.class, "fr");
+        bl = hardwareMap.get(DcMotorEx.class, "bl");
+        br = hardwareMap.get(DcMotorEx.class, "br");
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+        fr.setDirection(DcMotorSimple.Direction.FORWARD);
+        br.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 }
