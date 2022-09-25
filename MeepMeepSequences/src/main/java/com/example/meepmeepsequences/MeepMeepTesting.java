e1ae378948cd27cae12991239e7ce35e0067756f
jiru-shan <90534758+jiru-shan@users.noreply.github.com>
2022-09-25 14:45:19 -0700

    l bozo

--- MeepMeepSequences/src/main/java/com/example/meepmeepsequences/MeepMeepTesting.java
+++ MeepMeepSequences/src/main/java/com/example/meepmeepsequences/MeepMeepTesting.java
@@ -11,42 +11,219 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         // Declare a MeepMeep instance
         // With a field size of 800 pixels
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(700);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity blueLeft = new DefaultBotBuilder(meepMeep)
                 // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 // Option: Set theme. Default = ColorSchemeRedDark()
                 .setColorScheme(new ColorSchemeRedDark())
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-34.5, 62, Math.toRadians(0)))
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, 62, Math.toRadians(0)))
+
+                                //LEFT
+
+                                /*.strafeRight(50)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .strafeLeft(23)*/
+
+                                .lineToSplineHeading(new Pose2d(-35.5, 12, Math.toRadians(180)))
+                                //.lineToLinearHeading(new Pose2d(-35.5, 12, Math.toRadians(180)))
+                                //.setReversed(true)
+                               // .splineToLinearHeading(new Pose2d(-35.5, 12), Math.toRadians(180))
+
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+
+                                .strafeRight(23)
+                                .forward(20)
+
+                                .build()
 
 
+
+
+                );
+
+        RoadRunnerBotEntity blueRight = new DefaultBotBuilder(meepMeep)
+                // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                // Option: Set theme. Default = ColorSchemeRedDark()
+                .setColorScheme(new ColorSchemeRedDark())
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(35, 62, Math.toRadians(0)))
                                 .strafeRight(50)
-                                .back(21.5)
+
                                 .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .strafeLeft(23)
+
+                                /*.lineToSplineHeading(new Pose2d(35.5, 12, Math.toRadians(180)))
+                                //.lineToLinearHeading(new Pose2d(-35.5, 12, Math.toRadians(180)))
+                                //.setReversed(true)
+                                // .splineToLinearHeading(new Pose2d(-35.5, 12), Math.toRadians(180))
 
                                 .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
                                 .forward(21.5)
 
-                                .back(21.5)
-                                .forward(21.5)
-
-                                .back(21.5)
-                                .forward(21.5)
-
-                                .back(21.5)
-                                .forward(21.5)
+                                .strafeRight(23)
+                                .forward(20)*/
 
                                 .build()
                 );
 
+        RoadRunnerBotEntity redRight = new DefaultBotBuilder(meepMeep)
+                // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                // Option: Set theme. Default = ColorSchemeRedDark()
+                .setColorScheme(new ColorSchemeRedDark())
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(35, -62, Math.toRadians(0)))
+                                /*.strafeLeft(50)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .strafeRight(23)*/
+
+                                .lineToSplineHeading(new Pose2d(35.5, -12, Math.toRadians(180)))
+                                //.lineToLinearHeading(new Pose2d(-35.5, 12, Math.toRadians(180)))
+                                //.setReversed(true)
+                                // .splineToLinearHeading(new Pose2d(-35.5, 12), Math.toRadians(180))
+
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+                                .back(21.5)
+
+                                .forward(21.5)
+
+                                .strafeLeft(23)
+                                .forward(20)
+
+                                .build()
+                );
+
+        RoadRunnerBotEntity redLeft = new DefaultBotBuilder(meepMeep)
+                // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                // Option: Set theme. Default = ColorSchemeRedDark()
+                .setColorScheme(new ColorSchemeRedDark())
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, -62, Math.toRadians(0)))
+
+                                //LEFT
+
+                                .strafeLeft(50)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .strafeRight(23)
+
+                                /*.lineToSplineHeading(new Pose2d(-35.5, -12, Math.toRadians(180)))
+                                //.lineToLinearHeading(new Pose2d(-35.5, 12, Math.toRadians(180)))
+                                //.setReversed(true)
+                                // .splineToLinearHeading(new Pose2d(-35.5, 12), Math.toRadians(180))
+
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+                                .forward(21.5)
+
+                                .back(21.5)
+
+                                .strafeLeft(23)
+                                .forward(20)*/
+
+                                .build()
+
+
+
+
+                );
+
         // Set field image
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_DARK)
                 .setDarkMode(true)
                 // Background opacity from 0-1
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(blueLeft)
+                .addEntity(blueRight)
+                .addEntity(redRight)
+                .addEntity(redLeft)
                 .start();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/colorDetection.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.pipelines;
+
+public class colorDetection
+{
+
+}
