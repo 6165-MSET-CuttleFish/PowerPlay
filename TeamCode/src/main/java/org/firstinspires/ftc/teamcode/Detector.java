710160e94e98c3c4e174124520d8d21894db1bd9
SreyashDasSarma <sreyash.dassarma@gmail.com>
2022-10-02 13:54:01 -0700

    Update Detector.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detector.java
@@ -20,19 +20,20 @@ public class Detector extends OpenCvPipeline {
     }
     private Location location = Location.MIDDLE;
     FtcDashboard dashboard = FtcDashboard.getInstance();
-    // find and set the three regions of interest
+    // find and set the regions of interest
 
-    public static Rect POS_BLUE = new Rect(5, 5, 100, 230);
+    public static Rect POS_1_BLUE = new Rect(100, 5, 57, 230);
+    public static  Rect POS_2_BLUE = new Rect(163, 5, 57, 230);
 
     //Find numbers for actual place
 
-    public static int blueHLow = 50;
-    public static int blueSLow = 100;
-    public static int blueVLow = 30 ;
+    public static int blueHLow = 0;
+    public static int blueSLow = 0;
+    public static int blueVLow = 100 ;
 
-    public static int blueHHigh = 100;
+    public static int blueHHigh = 255;
     public static int blueSHigh = 255;
-    public static int blueVHigh = 255;
+    public static int blueVHigh = 160;
 
     public static boolean returnBlack = true;
     private double boxsize =0;
@@ -47,10 +48,21 @@ public class Detector extends OpenCvPipeline {
         highHSV = new Scalar(blueHHigh, blueSHigh, blueVHigh);
         Core.inRange(mat, lowHSV, highHSV, mat);
 
-        rectangle(mat, POS_BLUE, new Scalar(255, 255, 255));
-        rectangle(input,POS_BLUE, new Scalar(255, 255, 255));
+        rectangle(mat, POS_1_BLUE, new Scalar(255, 255, 255));
+        rectangle(mat, POS_2_BLUE, new Scalar(255, 255, 255));
+        rectangle(input,POS_1_BLUE, new Scalar(255, 255, 255));
+        rectangle(input,POS_2_BLUE, new Scalar(255, 255, 255));
 
-        double leftValue = Core.sumElems(mat.submat(POS_BLUE)).val[0]/(POS_BLUE.height*POS_BLUE.width*255);
+        double leftValue = Core.sumElems(mat.submat(POS_1_BLUE)).val[0]/(POS_1_BLUE.height*POS_1_BLUE.width*255);
+        double rightValue = Core.sumElems(mat.submat(POS_2_BLUE)).val[0]/(POS_2_BLUE.height*POS_2_BLUE.width*255);
+        if(leftValue-rightValue>0.05){
+            location=Location.LEFT;
+        }else if(leftValue-rightValue<-0.05){
+            location=Location.RIGHT;
+        }else{
+            location=Location.MIDDLE;
+        }
+        boxsize =Math.round(((leftValue+rightValue)*50));
         return returnBlack ? mat : input;
 
     }
