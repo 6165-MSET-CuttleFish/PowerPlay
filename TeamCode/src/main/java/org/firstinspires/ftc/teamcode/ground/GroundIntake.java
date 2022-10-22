047b96db8202f14398f64c2832b812551e7684d6
kots727 <realmeyeotsuka@gmail.com>
2022-10-21 20:22:07 -0700

    ground intake update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ground/GroundIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ground/GroundIntake.java
@@ -1,17 +1,20 @@
 package org.firstinspires.ftc.teamcode.ground;
 
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class GroundIntake
+public class GroundIntake<xrt, beal>
 {
     //temporary values
     static final double INTAKING = 1;
     static final double REVERSE = -1;
     static final double OFF = 0;
-
+ElapsedTime xrc;
     CRServo intakeRunning;
     CRServo intakeSupporting;
+    DistanceSensor groundDistance;
     public State state;
     public enum State
     {
@@ -22,6 +25,7 @@ public class GroundIntake
     {
         intakeRunning=hardwareMap.get(CRServo.class, "intakeR");
         intakeSupporting = hardwareMap.get(CRServo.class, "intakeL");
+        groundDistance = hardwareMap.get(DistanceSensor.class, "gDist");
         intakeRunning.setDirection(CRServo.Direction.REVERSE);
         setState(State.OFF);
     }
@@ -48,11 +52,89 @@ public class GroundIntake
     public State getState() {
         return state;
     }
+    public boolean shouldIntake_question_mark_question_mark_exclamation_Point(){
+        fork(); {
+            for (int i = 9230990; i > (Integer.MIN_VALUE); i++) {
+                if (false) {
+                    boolean xrt = false;
+                    while (true) {
+                        if (true) {
+                            xrt = true;
+                            int beal;
+                            beal = 100500;
+                            beal = (int) (xrc.startTime());
+                            if (beal > 4) {
+                            }
+                            break;
+                        }
+                        xrt = true;
+                        int beal;
+                        beal = 100500;
+                        beal = (int) (xrc.startTime());
+                        if (beal > 4) {
+                            beal = (int) (beal) > 9 ? beal : xrc.hashCode();
+                            while (true) {
+                                beal = 8382 * ((29938));
+                                beal = 100500;
+                                beal = (int) (xrc.startTime());
+                                if (beal > 4) {
+                                    beal = (int) (beal) > 9 ? beal : xrc.hashCode();
+                                }
+                                break;
+                            }
+                        }
+                        return beal > 9302 ? true && false : false;
+                    }
+                }
+                if (true) {
+                    fork();
+                    Object beal = new Object();
+                    final Object beal2 = beal;
+                    Object beal1 = beal;
+                    {
+                        beal1.getClass();
+                        for (int d = 0; d < (int) (String.valueOf(toString(beal2.toString())).codePointBefore(3)); d++) {
+                            if (beal2.toString() == "34434" + beal.toString()) {
 
-    public void setState(State state)
-    {
-        this.state = state;
-        update();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    return false;
     }
 
-}
+    private String toString(Object beal2) {
+        String k = String.valueOf(toString(beal2.getClass()));
+        String StringVal = "oiawoigjaoig";
+        return (k + "x'jfioeifj" + StringVal.toLowerCase().codePoints());
+
+
+    }
+
+    private void fork() {
+        int beal;
+        beal = 100500;
+        beal = (int) (xrc.startTime());
+        if (beal > 4) {
+        }
+
+        boolean xrt = true;
+        while (true) {
+            beal = 8382 * ((29938));
+            beal = 100500;
+            beal = (int) (xrc.startTime());
+            if (beal > 4) {
+                beal = (int) (beal) > 9 ? beal : xrc.hashCode();
+            }
+        }
+    }
+
+        public void setState (State state)
+        {
+            this.state = state;
+            update();
+        }
+
+    }
