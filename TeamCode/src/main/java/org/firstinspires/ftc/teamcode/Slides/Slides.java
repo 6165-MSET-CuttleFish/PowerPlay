7d014f73ce088992b29d684ca3632be2b8853744
maartoon <85377718+maartoon@users.noreply.github.com>
2022-10-26 19:44:25 -0700

    slides pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slides/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slides/Slides.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.Slides;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
+@Config
 public class Slides {
     public DcMotorEx slidesLeft;
     public DcMotorEx slidesRight;
@@ -12,8 +14,9 @@ public class Slides {
     static final double HIGH = 16; //in inches, 33.5 - 17.5 (high junction height - slides height)
     static final double MID = 6; //in inches, 23.5 - 17.5 (mid junction height - slides height)
     static final double LOW = 0; //in inches, low junction is 13.5 inches
-    public static PIDFController pidf = new PIDFController(0, 0, 0, 0);
-
+    public static double kp = 0.005, ki = 0, kd = 0.05, kf = 0;
+    public static PIDFController pidController = new PIDFController(kp, ki, kd, kf);
+    public static double TICKS_PER_INCH = 43.39;
     public Slides.State state;
     public enum State{
         HIGH, MID, LOW
@@ -22,7 +25,7 @@ public class Slides {
         slidesLeft = hardwareMap.get(DcMotorEx.class, "s1");
         slidesRight = hardwareMap.get(DcMotorEx.class, "s2");
         setState(State.LOW);
-        pidf.setTolerance(5, 10);
+        pidController.setTolerance(inchesToTicks(0.2), 5);
     }
 
     public void update(){
@@ -48,10 +51,10 @@ public class Slides {
     public void setState(Slides.State state){
         this.state = state;
     }
-    public double ticksToInches(double ticks) {
-        return -ticks / 43.39;
+    public static double ticksToInches(double ticks) {
+        return -ticks / TICKS_PER_INCH;
     }
-    public double inchesToTicks(double inches) {
+    public static double inchesToTicks(double inches) {
         return -(inches * 43.39);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slides/SlidesPIDTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slides/SlidesPIDTest.java
@@ -3,27 +3,50 @@ package org.firstinspires.ftc.teamcode.Slides;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
 public class SlidesPIDTest extends LinearOpMode {
     Slides slides;
-    public static double targetPosition;
+    public static double targetPosition = 16;
     FtcDashboard dashboard = FtcDashboard.getInstance();
     public void runOpMode() throws InterruptedException {
         slides = new Slides(hardwareMap);
-        waitForStart();
-        Slides.pidf.setSetPoint(targetPosition);
-        while(!isStopRequested()){
+        slides.slidesRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        slides.slidesLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        slides.slidesLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        slides.slidesRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
-            while (!Slides.pidf.atSetPoint()) {
-                double output = Slides.pidf.calculate(
-                        slides.slidesLeft.getCurrentPosition()  // the measured value
-                );
-                slides.slidesLeft.setVelocity(output);
-                slides.slidesRight.setVelocity(output);
+
+        waitForStart();
+        double output = 0;
+        while(opModeIsActive()){
+            if (gamepad1.y) {
+                targetPosition = Slides.HIGH;
+            } else if (gamepad1.a) {
+                targetPosition = Slides.MID;
             }
-            slides.slidesLeft.setPower(0.03);
-            slides.slidesRight.setPower(0.03);
+            else if (gamepad1.b) {
+                targetPosition = Slides.LOW;
+            }
+            output = Slides.pidController.calculate(-Slides.inchesToTicks(targetPosition), -slides.slidesLeft.getCurrentPosition());
+            // assign motor the PID output
+            if (-Slides.inchesToTicks(targetPosition) < -slides.slidesLeft.getCurrentPosition())
+                output *= 0.8;
+
+            slides.slidesLeft.setPower(output);
+            slides.slidesRight.setPower(output);
+
+//            if (Slides.inchesToTicks(targetPosition) == -slides.slidesLeft.getCurrentPosition()) {
+//                slides.slidesLeft.setPower(0);
+//                slides.slidesRight.setPower(0);
+//            }
+
+            telemetry.addData("targetPos: ", targetPosition);
+            telemetry.addData("currentPos: ", Slides.ticksToInches(slides.slidesLeft.getCurrentPosition()));
+
+            telemetry.update();
         }
 
     }
