aacb3ee9ae87821d4a251d88ebaa8c35b27525bd
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2022-11-13 16:48:39 -0800

    right side auto 1+0 pathing done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideAuto.java
@@ -5,32 +5,87 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.Slides.Slides;
+import org.firstinspires.ftc.teamcode.Transfer.Intake;
+import org.firstinspires.ftc.teamcode.Transfer.vfourb;
+import org.firstinspires.ftc.teamcode.Turret.Detector;
+import org.firstinspires.ftc.teamcode.Turret.Turret;
+import org.firstinspires.ftc.teamcode.ground.GroundIntake;
+import org.openftc.easyopencv.OpenCvWebcam;
+
 @Autonomous
 public class RightSideAuto extends LinearOpMode {
     Robot robot;
-    Pose2d startPose = new Pose2d(0,0,0);
+    Intake intake;
+    Slides slides;
+    vfourb fourbar;
+    GroundIntake groundIntake;
+    Turret turret;
+    Detector detector1;
+    OpenCvWebcam webcam;
+    Pose2d startPose = new Pose2d(0,0,Math.toRadians(-45));
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot = new Robot(hardwareMap);
-        Trajectory preload1 = robot.trajectoryBuilder(new Pose2d())
-                .lineToConstantHeading(new Vector2d(24, -2)).build();
+        intake = robot.intake;
+        slides = robot.slides;
+        fourbar = robot.fourbar;
+        groundIntake = robot.groundIntake;
+        turret = robot.turret;
+        turret.turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turret.turretMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fourbar.setState(vfourb.State.PRIMED);
+        Trajectory preload1 = robot.trajectoryBuilder(robot.getPoseEstimate())
+
+                .lineToConstantHeading(new Vector2d(-24, 2))
+
+                .build();
 
         Trajectory preload2 = robot.trajectoryBuilder(preload1.end())
-                .strafeRight(36)
-
+                .lineToConstantHeading(new Vector2d(-24, 38))
+                .addDisplacementMarker(2, ()->{
+                            slides.setState(Slides.State.HIGH);
+                            fourbar.setState(vfourb.State.ALIGN_POSITION);
+                        }
+                )
                 .build();
         Trajectory preload3 = robot.trajectoryBuilder(preload2.end())
-                .forward(2)
+
+                .lineToConstantHeading(new Vector2d(-29,38), robot.getVelocityConstraint(6, 5.939, 14.48),
+                        robot.getAccelerationConstraint(45))
+                .addTemporalMarker(1.5,()->{
+                    fourbar.setState(vfourb.State.DEPOSIT_POSITION);
+
+                })
+                .addTemporalMarker(3, ()->{
+                    intake.setState(Intake.State.DEPOSITING);
+                })
 
                 .build();
+        Trajectory preload4 = robot.trajectoryBuilder(preload3.end())
+                .lineToConstantHeading(new Vector2d(-22,50))
+                .addDisplacementMarker(2,()->{
+                    fourbar.setState(vfourb.State.PRIMED);
+                    intake.setState(Intake.State.OFF);
+                    slides.setState(Slides.State.BOTTOM);
+                })
+                .build();
+
+
+
         waitForStart();
         if(isStopRequested()) return;
+
         robot.setPoseEstimate(startPose);
+        robot.turn(Math.toRadians(45));
         robot.followTrajectory(preload1);
         robot.followTrajectory(preload2);
         robot.followTrajectory(preload3);
+        robot.followTrajectory(preload4);
+        while (!isStopRequested() && opModeIsActive()) ;
     }
 }
