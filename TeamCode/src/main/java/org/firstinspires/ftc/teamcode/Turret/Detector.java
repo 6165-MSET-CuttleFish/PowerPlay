a465ed61214603b7489ea8a9d466f2314f8594df
SreyashDasSarma <sreyash.dassarma@gmail.com>
2022-11-18 18:21:58 -0800

    Auton Paths (w/ new position)+ AutoAlign

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turret/Detector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turret/Detector.java
@@ -22,8 +22,8 @@ public class Detector extends OpenCvPipeline {
     FtcDashboard dashboard = FtcDashboard.getInstance();
     // find and set the regions of interest
 
-    public static Rect POS_1_BLUE = new Rect(100, 50, 57, 50);
-    public static Rect POS_2_BLUE = new Rect(163, 50, 57, 50);
+    public static Rect POS_1_BLUE = new Rect(50, 50, 57, 50);
+    public static Rect POS_2_BLUE = new Rect(113, 50, 57, 50);
 
     //Find numbers for actual place
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideAutoWow.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.Slides.Slides;
+import org.firstinspires.ftc.teamcode.Transfer.Intake;
+import org.firstinspires.ftc.teamcode.Transfer.vfourb;
+import org.firstinspires.ftc.teamcode.Turret.Detector;
+import org.firstinspires.ftc.teamcode.Turret.Turret;
+import org.firstinspires.ftc.teamcode.ground.GroundIntake;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous
+public class RightSideAutoWow extends LinearOpMode {
+    Robot robot;
+    Intake intake;
+    Slides slides;
+    vfourb fourbar;
+    GroundIntake groundIntake;
+    Turret turret;
+    Detector detector1;
+    OpenCvWebcam webcam;
+    Pose2d startPose = new Pose2d(-40,65,Math.toRadians(270));
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot(hardwareMap);
+        intake = robot.intake;
+        slides = robot.slides;
+        fourbar = robot.fourbar;
+        groundIntake = robot.groundIntake;
+        turret = robot.turret;
+        turret.turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turret.turretMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fourbar.setState(vfourb.State.PRIMED);
+        Trajectory preload1 = robot.trajectoryBuilder(robot.getPoseEstimate())
+
+                .lineToConstantHeading(new Vector2d(-12, 63))
+                .lineToConstantHeading(new Vector2d(-12, 12))
+
+                .build();
+
+        waitForStart();
+        if(isStopRequested()) return;
+
+        robot.setPoseEstimate(startPose);
+        robot.followTrajectory(preload1);
+        while (!isStopRequested() && opModeIsActive()) ;
+    }
+}
