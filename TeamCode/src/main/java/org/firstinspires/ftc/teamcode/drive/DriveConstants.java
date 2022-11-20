37d8eaf7d502684cd2db7f3a87f529b06855fced
jiru-shan <90534758+jiru-shan@users.noreply.github.com>
2022-11-20 11:47:08 -0800

    feedforward constants update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0140;
-    public static double kA = 0.003;
-    public static double kStatic = 0.001;
+    public static double kV = 0.0148;
+    public static double kA = 0.0038;
+    public static double kStatic = 0.0015;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
