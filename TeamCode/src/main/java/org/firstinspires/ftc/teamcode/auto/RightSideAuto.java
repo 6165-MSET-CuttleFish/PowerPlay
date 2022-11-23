d4e43c9097ff4f67941a7b260aa9493d6f93cf4a
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2022-11-22 23:55:31 -0800

    right side 1+1 update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideAuto.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.profile.VelocityConstraint;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
@@ -29,7 +30,7 @@ public class RightSideAuto extends LinearOpMode {
     Detector detector1;
     OpenCvWebcam webcam;
     Pose2d startPose = new Pose2d(-38,61,Math.toRadians(270));
-
+    double timer = 0;
     @Override
     public void runOpMode() throws InterruptedException {
         robot = new Robot(hardwareMap);
@@ -69,40 +70,87 @@ public class RightSideAuto extends LinearOpMode {
                     turret.setState(Turret.State.ZERO);
 
                 })
-                .lineToConstantHeading(new Vector2d(-40, 10))
+                .lineToConstantHeading(new Vector2d(-40, 11.5))
 
                 .build();
-        Trajectory preload3 = robot.trajectoryBuilder(preload2.end())
-                .lineToLinearHeading(new Pose2d(-42,10,Math.toRadians(176)))
-                .addDisplacementMarker(0.5, ()->{
-                    intake.setState(Intake.State.OFF);
-                })
-                .build();
-        Trajectory initCycle = robot.trajectoryBuilder(new Pose2d(-42,10,Math.toRadians(180)))
-                .lineToConstantHeading(new Vector2d(-62,8))
+
+        Trajectory initCycle = robot.trajectoryBuilder(new Pose2d(-40,10,Math.toRadians(180)))
+                .lineToConstantHeading(new Vector2d(-63.5,12))
                 .addDisplacementMarker(2, ()->{
                     slides.setState(Slides.State.LOW_DROP);
-                    //groundIntake.setState(GroundIntake.State.INTAKING);
+                    groundIntake.setState(GroundIntake.State.OFF);
+                    intake.setState(Intake.State.INTAKING);
                 })
                         .build();
+        Trajectory cycleDropOff1 = robot.trajectoryBuilder(initCycle.end())
+                .lineToConstantHeading(new Vector2d(-24.25,12.5),
+                        robot.getVelocityConstraint(30, 5.939, 14.48),
+                        robot.getAccelerationConstraint(23))
+                .addDisplacementMarker(2, ()->{
 
+                    turret.setState(Turret.State.LEFT);
+                    slides.setState(Slides.State.MID_DROP);
+                    //fourbar.setState(vfourb.State.ALIGN_POSITION);
+                })
+                .build();
+        Trajectory cycleIntake1 = robot.trajectoryBuilder(cycleDropOff1.end())
+
+                .lineToConstantHeading(new Vector2d(-63.5,12),
+                        robot.getVelocityConstraint(30, 5.939, 14.48),
+                        robot.getAccelerationConstraint(23))
+
+                .addDisplacementMarker(0, ()->{
+                    turret.setState(Turret.State.ZERO);
+                    slides.setState(Slides.State.BOTTOM);
+                    //groundIntake.setState(GroundIntake.State.OFF);
+                    intake.setState(Intake.State.INTAKING);
+
+                })
+                .addDisplacementMarker(5, ()->{
+                    slides.setState(Slides.State.LOW_DROP);
+                })
+                .build();
         /*Trajectory cycleIntake = robot.trajectoryBuilder(preload3.end())
                         .lineToConstantHeading(new Vector2d(-48,10))
                                 .build();*/
 
         waitForStart();
         if(isStopRequested()) return;
-
+        timer = System.currentTimeMillis();
+        //preload
         robot.setPoseEstimate(startPose);
         robot.followTrajectory(preload1);
         robot.followTrajectory(preload2);
-        robot.turn(Math.toRadians(-90));
-
-        //robot.followTrajectory(preload3);
-        //robot.setPoseEstimate(new Pose2d(-42,10,Math.toRadians(180)));
-        //robot.followTrajectory(initCycle);
-        //robot.updatePoseEstimate();
-        //robot.followTrajectory(cycleIntake);
+        robot.turn(Math.toRadians(-100));
+        //1st cycle
+        robot.followTrajectory(initCycle);
+        cycleIntake();
+        robot.followTrajectory(cycleDropOff1);
+        cycleDeposit();
+        //2nd cycle
+        /*
+        robot.followTrajectory(cycleIntake1);
+        cycleIntake();
+        robot.followTrajectory(cycleDropOff1);
+        cycleDeposit();
+        //3rd cycle
+        robot.followTrajectory(cycleIntake1);
+        cycleIntake();
+        robot.followTrajectory(cycleDropOff1);
+        cycleDeposit();*/
         while (!isStopRequested() && opModeIsActive()) ;
+    }
+    private void cycleIntake(){
+        robot.fourbar.setState(vfourb.State.INTAKE_POSITION);
+        timer = System.currentTimeMillis();
+        while(System.currentTimeMillis()-1500 < timer){}
+        fourbar.setState(vfourb.State.ALIGN_POSITION);
+    }
+    private void cycleDeposit(){
+        robot.fourbar.setState(vfourb.State.DEPOSIT_POSITION);
+        intake.setState(Intake.State.DEPOSITING);
+        timer = System.currentTimeMillis();
+        while(System.currentTimeMillis()-375 < timer){}
+        robot.fourbar.setState(vfourb.State.PRIMED);
     }
 }
