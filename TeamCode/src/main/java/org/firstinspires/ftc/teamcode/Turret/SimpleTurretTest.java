68256d4279e6b3a531651d204f47a5152e657709
maartoon <85377718+maartoon@users.noreply.github.com>
2022-12-28 17:45:20 -0800

    Update SimpleTurretTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turret/SimpleTurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turret/SimpleTurretTest.java
@@ -1,52 +1,31 @@
 package org.firstinspires.ftc.teamcode.Turret;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.Transfer.vfourb;
+import org.firstinspires.ftc.teamcode.util.Encoder;
 
+@TeleOp
 public class SimpleTurretTest extends LinearOpMode {
-    Turret turret;
-    Robot robot;
+    DcMotorEx turretMotor;
+    Encoder encoder;
     @Override
     public void runOpMode() throws InterruptedException {
-        robot = new Robot(this, true);
-        turret = robot.turret;
-        boolean turretStop = false;
-        while(!isStarted()) {
-            telemetry.addData("encoder", turret.encoder.getCurrentPosition());
+        turretMotor = hardwareMap.get(DcMotorEx.class, "hturret");
+        encoder=new Encoder(hardwareMap.get(DcMotorEx.class, "hturret"));
+        turretMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            turretMotor.setPower(gamepad1.right_stick_x);
+            telemetry.addData("ticks: ", encoder.getCurrentPosition());
             telemetry.update();
         }
-        waitForStart();
 
-        while(opModeIsActive()) {
-            if(gamepad1.a) {
-                turret.setState(Turret.State.RIGHT);
-            }
-            if(gamepad1.b) {
-                turret.setState(Turret.State.LEFT);
-            }
-            if(gamepad1.y) {
-                turret.setState(Turret.State.ZERO);
-            }
-
-            if (Math.abs(gamepad2.right_stick_x) > 0) {
-                turret.setState(Turret.State.MANUAL);
-                turret.turretMotor.setPower(gamepad2.right_stick_x);
-                turretStop = true;
-            }
-            if (turretStop && gamepad2.right_stick_x == 0) {
-                turret.setState(Turret.State.MANUAL);
-                turret.turretMotor.setPower(0);
-                turretStop = false;
-            }
-            //turret.update();
-            telemetry.addData("State", turret.getState());
-            telemetry.addData("power", turret.motorOil);
-            telemetry.addData("encoder", turret.encoder.getCurrentPosition());
-            //telemetry.addData("pid", turret.pidMotorOil);
-            telemetry.update();
         }
     }
-}
