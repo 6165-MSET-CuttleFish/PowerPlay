7743fca92f185b5d31e3d8f63cac726d5269a582
maartoon <85377718+maartoon@users.noreply.github.com>
2023-01-06 13:31:09 -0800

    Update HSTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/transfer/HSTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/transfer/HSTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Transfer;
+package org.firstinspires.ftc.teamcode.modules.transfer;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
