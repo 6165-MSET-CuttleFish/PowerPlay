17bffcc98602d2df1ff12df92fc591ff83dfcb2c
jiru-shan <90534758+jiru-shan@users.noreply.github.com>
2023-01-06 18:27:11 -0800

    lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detection/pipelines/Autoalign.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detection/pipelines/Autoalign.java
@@ -8,6 +8,12 @@ public class Autoalign extends OpenCvPipeline
     @Override
     public Mat processFrame(Mat input)
     {
+        //preprocessing(increase contrast, blur, etc.)
+        //threshold for colors that are wanted(red, blue, yellow)
+        //process that to make objects group together
+        //process for blobs
+        //use blob characteristics to identify correct poles & identify which pole aligning to
+        //Determine distance from said blob/pole
         return null;
     }
 }
