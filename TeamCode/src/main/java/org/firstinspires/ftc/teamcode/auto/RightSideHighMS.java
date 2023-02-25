786bebb9a3a66af55b213e692f81a04103d31dea
jiru-shan <90534758+jiru-shan@users.noreply.github.com>
2023-02-24 20:27:37 -0800

    reset duty lets go

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideHighMS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RightSideHighMS.java
@@ -68,7 +68,7 @@ public class RightSideHighMS extends LinearOpMode{
         robot.sideOdo.setPosition(sideOdomServoPos);
         robot.midOdo.setPosition(odomServoPos);
         Trajectory preload1 = robot.trajectoryBuilder(startPose)
-                .lineToConstantHeading(new Vector2d(-36.7, 13.5))
+                .lineToConstantHeading(new Vector2d(-37, 12.5))
 
                 .addTemporalMarker(0,()->{
                     slides.setState(Slides.State.HIGH);
@@ -81,7 +81,7 @@ public class RightSideHighMS extends LinearOpMode{
 
                 .build();
         Trajectory preload2 = robot.trajectoryBuilder(preload1.end())
-                .lineToLinearHeading(new Pose2d(-35.25,13.5, Math.toRadians(174.5)))
+                .lineToLinearHeading(new Pose2d(-35.25,13, Math.toRadians(174.5)))
                 .addTemporalMarker(0.1,()->{
                     turret.setState(Turret.State.ZERO);
                     groundIntake.setState(GroundIntake.State.INTAKING);
@@ -91,7 +91,7 @@ public class RightSideHighMS extends LinearOpMode{
                     slides.setState(Slides.State.CYCLE0);
                 })
                 .build();
-        Trajectory initIntake = robot.trajectoryBuilder(new Pose2d(-35.25,13.5, Math.toRadians(180)))
+        Trajectory initIntake = robot.trajectoryBuilder(new Pose2d(-35.25,13, Math.toRadians(180)))
                 .lineToConstantHeading(new Vector2d(-56.5, 13))
                 .addTemporalMarker(0.1, ()->{
                     deposit.setExtension(Deposit.ExtensionState.EXTEND);
@@ -101,7 +101,7 @@ public class RightSideHighMS extends LinearOpMode{
                 .build();
 
         Trajectory cycleDrop = robot.trajectoryBuilder(initIntake.end())
-                .lineToConstantHeading(new Vector2d(-37.2, 13.4),robot.getVelocityConstraint(58, 5.939, 13.44),
+                .lineToConstantHeading(new Vector2d(-36.4, 12.5),robot.getVelocityConstraint(58, 5.939, 13.44),
                         robot.getAccelerationConstraint(60))
                 .addTemporalMarker(0, ()->{
                     slides.setState(Slides.State.CYCLE_HIGH);
@@ -114,7 +114,7 @@ public class RightSideHighMS extends LinearOpMode{
         Trajectory cycleIntake = robot.trajectoryBuilder(cycleDrop.end())
                 .lineToConstantHeading(new Vector2d(-56.5, 13),robot.getVelocityConstraint(55, 5.939, 13.44),
                         robot.getAccelerationConstraint(60))
-                .addTemporalMarker(0.61, ()->{
+                .addTemporalMarker(0.7, ()->{
                     deposit.setExtension(Deposit.ExtensionState.EXTEND);
 
                 })
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/turret/AlignerAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/turret/AlignerAuto.java
@@ -22,8 +22,7 @@ public class AlignerAuto extends OpenCvPipeline {
     public static int factor=75;
     FtcDashboard dashboard = FtcDashboard.getInstance();
     // find and set the regions of interest
-
-    public static Rect POS_1_BLUE = new Rect(0, 10, 40, 30);
+public static Rect POS_1_BLUE = new Rect(0, 10, 40, 30);
     public static Rect POS_2_BLUE = new Rect(40, 10, 40, 30);
     public static Rect POS_3_BLUE = new Rect(80, 10, 40, 30);
     public static Rect POS_4_BLUE = new Rect(120, 10, 40, 30);
@@ -58,36 +57,36 @@ public class AlignerAuto extends OpenCvPipeline {
         highHSV = new Scalar(blueHHigh, blueSHigh, blueVHigh);
         Core.inRange(mat, lowHSV, highHSV, mat);
 
-        rectangle(mat, POS_1_BLUE, new Scalar(255, 255, 255));
+        //rectangle(mat, POS_1_BLUE, new Scalar(255, 255, 255));
         rectangle(mat, POS_2_BLUE, new Scalar(255, 255, 255));
         rectangle(mat, POS_3_BLUE, new Scalar(255, 255, 255));
         rectangle(mat, POS_4_BLUE, new Scalar(255, 255, 255));
         rectangle(mat, POS_5_BLUE, new Scalar(255, 255, 255));
         rectangle(mat, POS_6_BLUE, new Scalar(255, 255, 255));
         rectangle(mat, POS_7_BLUE, new Scalar(255, 255, 255));
-        rectangle(mat, POS_8_BLUE, new Scalar(255, 255, 255));
-        rectangle(input,POS_1_BLUE, new Scalar(255, 255, 255));
+        //rectangle(mat, POS_8_BLUE, new Scalar(255, 255, 255));
+        //rectangle(input,POS_1_BLUE, new Scalar(255, 255, 255));
         rectangle(input,POS_3_BLUE, new Scalar(255, 255, 255));
         rectangle(input,POS_4_BLUE, new Scalar(255, 255, 255));
         rectangle(input,POS_5_BLUE, new Scalar(255, 255, 255));
         rectangle(input,POS_6_BLUE, new Scalar(255, 255, 255));
         rectangle(input,POS_7_BLUE, new Scalar(255, 255, 255));
         rectangle(input,POS_2_BLUE, new Scalar(255, 255, 255));
-        rectangle(input,POS_8_BLUE, new Scalar(255, 255, 255));
+        //rectangle(input,POS_8_BLUE, new Scalar(255, 255, 255));
 
-        loc[0] = Core.sumElems(mat.submat(POS_1_BLUE)).val[0]/(POS_1_BLUE.height*POS_1_BLUE.width*255);
+        //loc[0] = Core.sumElems(mat.submat(POS_1_BLUE)).val[0]/(POS_1_BLUE.height*POS_1_BLUE.width*255);
         loc[1] = Core.sumElems(mat.submat(POS_2_BLUE)).val[0]/(POS_2_BLUE.height*POS_2_BLUE.width*255);
         loc[2] = Core.sumElems(mat.submat(POS_3_BLUE)).val[0]/(POS_3_BLUE.height*POS_3_BLUE.width*255);
         loc[3] = Core.sumElems(mat.submat(POS_4_BLUE)).val[0]/(POS_4_BLUE.height*POS_4_BLUE.width*255);
         loc[4] = Core.sumElems(mat.submat(POS_5_BLUE)).val[0]/(POS_5_BLUE.height*POS_5_BLUE.width*255);
         loc[5] = Core.sumElems(mat.submat(POS_6_BLUE)).val[0]/(POS_6_BLUE.height*POS_6_BLUE.width*255);
         loc[6] = Core.sumElems(mat.submat(POS_7_BLUE)).val[0]/(POS_7_BLUE.height*POS_7_BLUE.width*255);
-        loc[7] = Core.sumElems(mat.submat(POS_8_BLUE)).val[0]/(POS_7_BLUE.height*POS_8_BLUE.width*255);
-        boxsize =Math.round(((loc[0]+loc[1]+loc[3]+loc[4]+loc[5]+loc[6]+loc[2])*50));
+        //loc[7] = Core.sumElems(mat.submat(POS_8_BLUE)).val[0]/(POS_7_BLUE.height*POS_8_BLUE.width*255);
+        boxsize =Math.round(((/*loc[0]*/+loc[1]+loc[3]+loc[4]+loc[5]+loc[6]+loc[2])*50));
         record=Math.abs(loc[3]-loc[4]);
         if(getShift()<restrict) {
             location= Location.MIDDLE;
-        }else if((loc[0]+loc[1]+loc[2]+loc[3])<(loc[5]+loc[6]+loc[4]+loc[7])){
+        }else if((/*loc[0]*/+loc[1]+loc[2]+loc[3])<(loc[5]+loc[6]+loc[4]/*loc[7]*/)){
             location= Location.RIGHT;
         }else{
             location= Location.LEFT;
@@ -111,6 +110,6 @@ public class AlignerAuto extends OpenCvPipeline {
     }
     public int getShift() {
         //Find shift
-        return (int)(-1*factor*(loc[0]*2.5+loc[1]*1.5+loc[2]*0.5+loc[3]*-0.5+loc[4]*-1.5+loc[5]*-2.5+loc[6]*-3.5));
+        return (int)(-1*factor*(/*loc[0]*2.5*/+loc[1]*2.5+loc[2]*1.5+loc[3]*0.5+loc[4]*-0.5+loc[5]*-1.5+loc[6]*-2.5));
     }
 }
