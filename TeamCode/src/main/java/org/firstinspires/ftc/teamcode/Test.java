314f5739bc9aca1c08c50db46dd35b854370719c
SreyashDasSarma <sreyash.dassarma@gmail.com>
2023-02-26 14:30:59 -0800

    Test class for imu

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.RobotTemp;
+
+import java.util.List;
+
+/**
+ * This is a simple teleop routine for testing localization. Drive the robot around like a normal
+ * teleop routine and make sure the robot's estimated pose matches the robot's actual pose (slight
+ * errors are not out of the ordinary, especially with sudden drive motions). The goal of this
+ * exercise is to ascertain whether the localizer has been configured properly (note: the pure
+ * encoder localizer heading may be significantly off if the track width has not been tuned).
+ */
+@TeleOp(group = "drive")
+public class Test extends LinearOpMode {
+    double prevHeading;
+    double biggestDelta;
+
+    private BNO055IMU imu;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        prevHeading=0;
+
+        //SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
+        waitForStart();
+        telemetry.addData("",imu.getAngularVelocity());
+    }
+}
